29167b2aa846144a1ab949e2b738770a6c5af8a4
Akshat Sachan <52987091+AkshatSachan@users.noreply.github.com>
2020-07-07 18:24:28 +0530

    Code for question highly divisible triangle number

---
+++ project-euler-12.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int counter=0;
+long long int sum=1;
+long long int num=2;
+int x=1;
+while(x)
+{
+    counter=0;
+    for(long long int i=1;i<=sqrt(sum);i++)
+    {
+        if(sum%i==0)
+        {
+            counter++;
+            if(sum%i!=i)
+                counter++;
+        }
+    }
+    if(counter>500)
+        break;
+    sum+=num;
+    num++;
+
+}
+cout<<sum<<endl;
+}
